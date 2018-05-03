eeb70c5d1fbe51d7e27d0916810cdbe8beee7a76
Жулдыза <alisherovna23@gmail.com>
2018-05-04 03:14:17 +0600

    database connection changed

--- src/main/java/com/example/biba/Model/Samsa.java
+++ src/main/java/com/example/biba/Model/Samsa.java
@@ -6,6 +6,7 @@ import javax.validation.constraints.Pattern;
 import java.util.List;
 
 @Entity
+@Table(name="Samsa")
 public class Samsa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
